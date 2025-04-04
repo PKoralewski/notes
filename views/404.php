17bd1943ee3935c6667f69205c724d074c8f59be
PKoralewski <pkoralewski1@gmail.com>
2025-04-04 10:22:34 +0200

    style: add heading 1 class in 404 view

--- views/404.php
+++ views/404.php
@@ -5,7 +5,7 @@
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1>Sorry. Not Found.</h1>
+        <h1 class="text-2xl font-bold">Sorry. Not Found.</h1>
         <a href="/" class='underline text-blue-500'>Go back to the home.</a>
     </div>
 </main>
