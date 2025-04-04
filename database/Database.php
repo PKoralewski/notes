f527ed1bfec47d179851a5ec26b127a1abe1e7c5
PKoralewski <pkoralewski1@gmail.com>
2025-04-04 10:56:14 +0200

    fix: add $this reference in query Database query function

--- database/Database.php
+++ database/Database.php
@@ -15,8 +15,8 @@ class Database {
 
     public function query($query, $params = []) 
     { 
-        $statement = $this->connection->prepare($query);
-        $statement->execute($params);
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
         
         return $this;
     }
